41b3fcc2493c1c49f9acd016f3382ada7f9219e7
Dan Wiechert <dan.wiechert@gmail.com>
2014-06-24 21:55:04 -0500

    Finished example 3.

---
+++ src/main/java/example3/Java7.java
@@ -0,0 +1,21 @@
+package example3;
+
+import java.util.List;
+
+import common.ListMaker;
+import common.Person;
+
+/**
+ * Shows mapping the java 7 way.
+ * 
+ * @author Dan Wiechert
+ */
+public class Java7 {
+	public static void main(final String[] args) {
+		final List<Person> persons = ListMaker.createPersons();
+		
+		for (final Person person : persons) {
+			System.out.println(person.getName().length());
+		}
+	}
+}
---
+++ src/main/java/example3/Java8.java
@@ -0,0 +1,20 @@
+package example3;
+
+import java.util.List;
+
+import common.ListMaker;
+import common.Person;
+
+/**
+ * Shows mapping the java 8 way.
+ * 
+ * @author Dan Wiechert
+ */
+public class Java8 {
+	public static void main(final String[] args) {
+		final List<Person> persons = ListMaker.createPersons();
+
+		persons.stream().map(p -> p.getName()).map(n -> n.length())
+				.forEach(nl -> System.out.println(nl));
+	}
+}

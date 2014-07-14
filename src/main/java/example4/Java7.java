e1222d0d1f4b61aed8163de1ef4d692e8a41da1e
Dan Wiechert <dan.wiechert@cerner.com>
2014-07-14 15:21:14 -0500

    Switched packages 4 and 5.

--- src/main/java/example4/Java7.java
+++ src/main/java/example4/Java7.java
@@ -1,46 +1,27 @@
 package example4;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import common.ListMaker;
 import common.Person;
 
 /**
- * Shows comparing the java 7 way.
+ * Shows how to implement interfaces the java 7 way.
  * 
  * @author Dan Wiechert
  */
 public class Java7 {
     public static void main(final String[] args) {
         final List<Person> persons = ListMaker.createPersons();
-        persons.addAll(ListMaker.createPersons2());
 
-        Collections.sort(persons, new PersonComparator());
-        
         for (final Person person : persons) {
-            System.out.println(person);
-        }
-    }
-
-    private static class PersonComparator implements Comparator<Person> {
-        public PersonComparator() {
-        }
-
-        @Override
-        public int compare(final Person p1, final Person p2) {
-            final int nameCompare = p1.getName().compareTo(p2.getName());
-            if (nameCompare != 0) {
-                return nameCompare;
-            }
-            
-            final int sexCompare = p1.getSex().compareTo(p2.getSex());
-            if (sexCompare != 0) {
-                return sexCompare;
-            }
-            
-            return p1.getEmail().compareTo(p2.getEmail());
+            final Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(person.getName());
+                }
+            };
+            runnable.run();
         }
     }
 }
--- src/main/java/example5/Java8.java
+++ src/main/java/example5/Java8.java
@@ -1,29 +1,28 @@
 package example5;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import common.ListMaker;
 import common.Person;
 
 /**
- * Shows how to implement interfaces the java 8 way.
+ * Shows comparing the java 8 way.
  * 
  * @author Dan Wiechert
  */
 public class Java8 {
     public static void main(final String[] args) {
         final List<Person> persons = ListMaker.createPersons();
+        persons.addAll(ListMaker.createPersons2());
 
         // This is a lambda expression for the interface of Comparator.
         // It is in the form of (argument list) -> body
-        persons.forEach(p -> {
-            final Runnable runnable = () -> {
-                // We can add multiple lines within a lambda expression with the curly brackets '{}'
-                // Though they're not truly needed for this example so it could've been just:
-                // final Runnable runnable = () -> System.out.println(p.getName());
-                System.out.println(p.getName());
-            };
-            runnable.run();
-        });
+        final Comparator<Person> byEmail = (p1, p2) -> p1.getEmail().compareTo(p2.getEmail());
+        // Sorting the list of persons by their name, then their sex, and then finally by email.
+        // The Person::getName is a new way to indicate what field of the class you want to use for comparing.
+        Collections.sort(persons, Comparator.comparing(Person::getName).thenComparing(Person::getSex).thenComparing(byEmail));
+        persons.forEach(p -> System.out.println(p));
     }
 }
